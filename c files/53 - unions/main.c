92f80b2be8269dc6b60639d30b97ac92ed2fb9c8
Leonardo Ennes <leonardoennes7@gmail.com>
2023-05-05 23:19:47 -0300

    learning Unions

--- c files/53 - unions/main.c
+++ c files/53 - unions/main.c
@@ -11,7 +11,66 @@
   it will be reflected to other members as well.
 
 */
+
+/*
+union abc {
+  int a;
+  char b;
+}var;
+
 int main(){
+  var.a = 65;
+  printf("a = %d\n", var.a);
+  printf("b = %c\n", var.b);
 
   return 0;
 }
+*/
+
+/*
+output:
+ a = 65
+ b = a
+
+*/
+
+
+// HOW TO DECIDE THE SIZE OF THE UNION
+
+/* SIZE OF THE UNION IS TAKEN ACCORDING TO THE SIZE OF THE LARGEST MEMBER OF THE UNION. */
+
+/* EXAMPLE
+
+union abc {
+ int a;
+ char b;
+ double c;
+ float d;
+};
+
+int main(){
+  printf("%ld", sizeof(union abc));
+
+  return 0;
+}
+*/
+// Output: 8 (bc its the largest)
+
+
+// ACCESSING MEMBERS USING POINTERS
+// WE CAN ACESS MEMBERS OF UNION THROUGH POINTERS BY USING THE ARROUW (->) OPERATOR
+
+union abc {
+  int a;
+  char b;
+};
+
+int main(){
+  union abc var;
+  var.a = 90;
+  union abc *p = &var;
+  printf("%d %c", p->a, p->b);
+  return 0;
+}
+
+// output 90 Z 
